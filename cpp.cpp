beed086353f653f05c78d5916ca6960f50ec5859
Kavindu Warnakulasuriya <86293207+akw2000@users.noreply.github.com>
2022-08-07 21:28:52 +0530

    Create cpp.cpp

---
+++ cpp.cpp
@@ -0,0 +1,46 @@
+// simple array traverse  
+for(auto& num : arr){
+sum += num;
+}
+ 
+// sum
+accumulate(v.begin(), v.end(), initial_sum);
+accumulate(a, a+n, initial_sum);
+ 
+// size of array
+int n = sizeof(arr) / sizeof(arr[0]);
+ 
+// init arr
+int arr[26] = { }; // initialize with zeros
+ 
+// sort
+sort (Vec.begin(), Vec.end());
+sort(arr, arr + n, greater<int>()); // decreasing order array
+ 
+// int array
+    int* parent = (int*)malloc(nodes* sizeof(int));
+ 
+// pass to function
+int findParent(int i, int* parent){}
+ 
+// max and min heap ( priority queues)
+    priority_queue<double> maxHeap;
+    priority_queue<double,vector<double>,greater<double> > minHeap;
+// queue
+#include <queue>
+    queue<int> q;
+    q.push(10);
+ 
+ 
+ios_base::sync_with_stdio(false);
+cin.tie(0); cout.tie(0);
+ 
+
+
+
+
+
+
+
+
+
